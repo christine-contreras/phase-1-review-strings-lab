88489e2a006abd873ceb8e5599fd72ad3c52371b
Christine Contreras <christineec23@yahoo.com>
2021-04-05 11:48:13 -0500

    commit tree 900bb75cac51f430b37e4d955ede66b31eb946c7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//slice (0,1) will take off the first letter of currentUser
+//you can add any expression inbetween brackets not just variables. You can do math etc.
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
